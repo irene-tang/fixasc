5fb99722f120d94ecc6fbff625a81c45a15e60a8
Irene <irenetang14@gmail.com>
2019-03-08 02:31:12 -0500

    obtain data analysis measures

--- src/edit_cond.py
+++ src/edit_cond.py
@@ -2,44 +2,61 @@ import sys, os
 import csv
 import numpy as np
 
+# declare output path
+OUTPUT_PATH = "../data/eyedry/edited_originals/"
+FILES_TO_PROCESS = [
+    "../data/eyedry/originals/firstPassIXS",
+    "../data/eyedry/originals/goPastIXS",
+    "../data/eyedry/originals/probFixationIXS",
+    "../data/eyedry/originals/probRegressionIXS",
+    "../data/eyedry/originals/firstFixationReg3IXS",
+]
+
 # check for correct commandline args
-if len(sys.argv) != 2:
-    print('incorrect usage: python3 dataanal.py [file]')
+# if len(sys.argv) != 2:
+#     print('incorrect usage: python3 dataanal.py [file]')
 
 # get name of file
-inputFilename = sys.argv[1]
+# inputFilename = sys.argv[1]
 
-# enter data as list
-with open(inputFilename, newline='') as readFile:
-    data = list(csv.reader(readFile))
+for inputFilename in FILES_TO_PROCESS:
+    # enter data as list
+    with open(inputFilename, newline='') as readFile:
+        data = list(csv.reader(readFile))
 
-# convert to numpy array
-# data = np.asarray(data)
+    # convert to numpy array
+    # data = np.asarray(data)
 
-# perform our conversions
-num_rows = len(data)
-num_cols = len(data[0])
-for i,row in enumerate(data):
-    for j, col in enumerate(row):
-        # replace missing values with 0
-        if data[i][j] == '':
-            data[i][j] = '0'
-        # edit the condition in 4th column
-        elif j == 3 and data[i][j] == '1':
-            data[i][j] = 'match_tap'
-        elif j == 3 and data[i][j] == '2':
-            data[i][j] = 'match_this'
-        elif j == 3 and data[i][j] == '3':
-            data[i][j] = 'clash_tap'
-        elif j == 3 and data[i][j] == '4':
-            data[i][j] = 'clash_this'
+    # perform our conversions
+    num_rows = len(data)
+    num_cols = len(data[0])
+    for i,row in enumerate(data):
+        for j, col in enumerate(row):
+            # replace missing values with 0
+            if data[i][j] == '':
+                data[i][j] = '0'
+            # edit the condition in 4th column
+            if j == 3 and data[i][j] == '1':
+                data[i][j] = 'match_tap'
+            elif j == 3 and data[i][j] == '2':
+                data[i][j] = 'match_this'
+            elif j == 3 and data[i][j] == '3':
+                data[i][j] = 'clash_tap'
+            elif j == 3 and data[i][j] == '4':
+                data[i][j] = 'clash_this'
+            # if condition value is zero but sequence value is non-zero,
+            # replace the sequence value with 0
+            elif j == 3 and data[i][j] == '0' and data[i][0] != 0:
+                data[i][0] = 0
 
-# save it to the output file
-outputFilename = inputFilename.split('.')[0] + '_edited'
-with open(outputFilename, 'w+') as writeFile:
-    writer = csv.writer(writeFile)
-    writer.writerows(data)
+    # save it to the output file
+    outputFilename = OUTPUT_PATH + inputFilename.rsplit('/',1)[-1] + '_edited'
+    print(outputFilename)
 
-# close files
-readFile.close()
-writeFile.close()
+    with open(outputFilename, 'w+') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerows(data)
+
+    # close files
+    readFile.close()
+    writeFile.close()
